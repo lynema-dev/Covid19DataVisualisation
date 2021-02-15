0bb27da3784ea8f3d0135e4e3622474506969e1c
Matthew Lynes <63749896+lynema-dev@users.noreply.github.com>
2021-02-15 16:12:38 +0000

    Add files via upload
    Additional chart types added.

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import pandas as pd
-import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
 from pandas.plotting import register_matplotlib_converters
+import seaborn as sns
 import os
+import numpy as np
+import math
+from datetime import datetime, timedelta
+
+location = 'United Kingdom'
+fieldname = 'new_cases'
+movingaverageupper = 28
+movingaveragelower = 7    
+location2 = 'United States'
+populationminimum = 10000000
+groupbyfield = 'location'
+xVariable = 'total_tests_per_thousand'
+yVariable = 'total_cases_per_million'
+thirdVariable = 'continent'
+chartbtype = 4
+booLog = False
+exportfile = 'c:\\temp\\covid19output.csv'
+
+correlationfields = ['total_cases_per_million', 'total_deaths_per_million', 'total_tests_per_thousand',
+        'mortality_rate', 'total_vaccinations_per_hundred', 'population_density', 
+        'median_age', 'gdp_per_capita', 'hospital_beds_per_thousand', 
+        'life_expectancy']
 
 def main():
-
-    fieldname = 'new_cases'
-    movingaverageupper = 28
-    movingaveragelower = 7
-    location = 'United Kingdom'
-    populationminimum = 20000000
-    groupbyfield = 'iso_subregion'
 
     datastream = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
     df = pd.read_csv(datastream, error_bad_lines=False)
@@ -25,7 +41,7 @@ def main():
     dfiso = pd.read_csv(isofile)
     df.iso_code = df.iso_code.astype(str)
     df.iso_code = df.iso_code.str.strip()
-    df['mortality'] = df['total_deaths'] / df['total_cases']
+    df['mortality_rate'] = df['total_deaths_per_million'] / df['total_cases_per_million']
     
     #join regional fields to iso_code
     df = pd.merge(df, dfiso, how='right', on='iso_code')
@@ -34,12 +50,74 @@ def main():
     register_matplotlib_converters()
     pd.options.mode.chained_assignment = None
 
-    movingaveragebarchartcomparison(location, movingaveragelower, movingaverageupper, fieldname, df, 
-        False, populationminimum, True, groupbyfield)
+    ChartProcess(chartbtype, df)
 
 
-#bar chart or relative moving averages
-def movingaveragebarchartcomparison(location, movingaveragelower, movingaverageupper, fieldname, df, 
+#to determine what chart type is run
+def ChartProcess(bType, df):
+
+    if bType == 1:
+        MovingAverageBarChartComparison(location, movingaveragelower, movingaverageupper, 
+        fieldname, df, False, populationminimum, True, groupbyfield)
+    elif bType == 2:
+        MovingAverageLineChartcomparison(location, movingaveragelower, movingaverageupper, 
+        fieldname, df, False)
+    elif bType == 3:
+         LineChartComparison(location, location2, fieldname, df, False)
+    elif bType == 4:
+         ScatterChart(xVariable, yVariable, thirdVariable, 
+         populationminimum, groupbyfield, df, True, booLog)
+    elif bType == 5:
+        CorrelationHeatMap(populationminimum, correlationfields, df, True)
+    else:
+        print ('Invalid Selection!')
+
+
+#scatter plot of two variables colour coded by third variable
+def ScatterChart(xVariable, yVariable, thirdVariable, populationminimum, groupbyfield, 
+    df, exporttocsv, booLog):
+
+    maxdate = (df['date']).max() - timedelta(days = 1)
+    maxdate = maxdate.strftime('%Y%m%d')
+    df['date'] = pd.to_datetime(df['date']).dt.strftime('%Y%m%d')
+    df2 = df[(df['date'] == maxdate) & (df['population'] > populationminimum)]
+
+    if booLog:
+        df2[xVariable] = np.log(df2[xVariable])
+        df2[yVariable] = np.log(df2[yVariable])
+    
+    sns.relplot(x=xVariable, y=yVariable, hue=thirdVariable, data=df2)
+    plt.show()
+
+    if exporttocsv:
+        df2.to_csv(exportfile)
+
+
+#correlation heatmap of multiple variables
+def CorrelationHeatMap(populationminimum, fields, df, exporttocsv):
+
+    maxdate = (df['date']).max() - timedelta(days = 1)
+    maxdate = maxdate.strftime('%Y%m%d')
+    df['date'] = pd.to_datetime(df['date']).dt.strftime('%Y%m%d')
+    df2 = df[(df['date'] == maxdate) & (df['population'] > populationminimum)]
+
+    df3 = df2[fields]
+    heatmap = sns.heatmap(df3.corr(), vmin=-1, vmax=1, annot=True, cmap='YlGnBu', cbar=True)
+    heatmap.set_yticklabels(heatmap.get_yticklabels(), rotation = 0, fontsize = 9)
+    heatmap.set_xticklabels(heatmap.get_xticklabels(), rotation = 0, fontsize = 7)
+    
+    ChartTitle = 'Covid-19 Correlation Heatmap (country populations > ' + str(populationminimum) + ')'
+    heatmap.set_title(ChartTitle, fontdict={'fontsize':14}, pad=12)
+
+    plt.xticks(rotation = 25)
+    plt.show()
+
+    if exporttocsv:
+        df3.to_csv(exportfile)
+
+
+#bar chart of relative moving averages
+def MovingAverageBarChartComparison(location, movingaveragelower, movingaverageupper, fieldname, df, 
     exporttocsv, populationminimum, byPerMillion, groupbyfield):
 
     maxdate = (df['date']).max()
@@ -86,12 +164,11 @@ def movingaveragebarchartcomparison(location, movingaveragelower, movingaverageu
     plt.show()
 
     if exporttocsv:
-        exportfile = 'c:\\temp\\covid19output1.csv'
         df2.to_csv(exportfile)
 
 
 #line chart of two country comparison
-def linechartcomparison(location, location2, fieldname, df,exporttocsv):
+def LineChartComparison(location, location2, fieldname, df, exporttocsv):
 
     df['date'] = pd.to_datetime(df['date'])
     df2 = df[df['location'] == location]
@@ -105,12 +182,11 @@ def linechartcomparison(location, location2, fieldname, df,exporttocsv):
     plt.show()
 
     if exporttocsv:
-        exportfile = 'c:\\temp\\covid19output2.csv'
         df2.to_csv(exportfile)
 
 
 #line chart of relative moving averages
-def movingaveragelinechartcomparison(location, movingaveragelower, movingaverageupper, fieldname,
+def MovingAverageLineChartcomparison(location, movingaveragelower, movingaverageupper, fieldname,
      df, exporttocsv):
 
     df2 = df[df['location'] == location]
@@ -126,7 +202,6 @@ def movingaveragelinechartcomparison(location, movingaveragelower, movingaverage
     plt.show()
 
     if exporttocsv:
-        exportfile = 'c:\\temp\\covid19output3.csv'
         df2.to_csv(exportfile)
 
 
@@ -134,42 +209,19 @@ if __name__ == '__main__':
 
     main()
 
-        #example implementations
-        #movingaveragebarchartcomparison(location, movingaveragelower, movingaverageupper, fieldname, df, 
-        #   False, populationminimum, True, groupbyfield)
-        #movingaveragelinechartcomparison(location, movingaveragelower, movingaverageupper, fieldname, df, False)
-        #linechartcomparison(location, 'World', fieldname, df, False)
+    #data fields available in file
+    #iso_code	continent	location	date	total_cases	new_cases	new_cases_smoothed	
+    # total_deaths	new_deaths	new_deaths_smoothed	total_cases_per_million	new_cases_per_million	
+    # new_cases_smoothed_per_million	total_deaths_per_million	new_deaths_per_million	
+    # new_deaths_smoothed_per_million	reproduction_rate	icu_patients	icu_patients_per_million	
+    # hosp_patients	hosp_patients_per_million	weekly_icu_admissions	weekly_icu_admissions_per_million	
+    # weekly_hosp_admissions	weekly_hosp_admissions_per_million	new_tests	total_tests	total_tests_per_thousand	
+    # new_tests_per_thousand	new_tests_smoothed	new_tests_smoothed_per_thousand	positive_rate	tests_per_case	
+    # tests_units	total_vaccinations	people_vaccinated	people_fully_vaccinated	new_vaccinations	
+    # new_vaccinations_smoothed	total_vaccinations_per_hundred	people_vaccinated_per_hundred	
+    # people_fully_vaccinated_per_hundred	new_vaccinations_smoothed_per_million	stringency_index	
+    # population	population_density	median_age	aged_65_older	aged_70_older	gdp_per_capita	
+    # extreme_poverty	cardiovasc_death_rate	diabetes_prevalence	female_smokers	male_smokers	
+    # handwashing_facilities	hospital_beds_per_thousand	life_expectancy	human_development_index
 
-        #data fields available in file
-        #iso_code
-        #location
-        #date
-        #total_cases
-        #new_cases
-        #total_deaths
-        #new_deaths
-        #total_cases_per_million
-        #new_cases_per_million
-        #total_deaths_per_million
-        #new_deaths_per_million
-        #total_tests
-        #new_tests
-        #total_tests_per_thousand
-        #new_tests_per_thousand
-        #new_tests_smoothed
-        #new_tests_smoothed_per_thousand
-        #tests_units
-        #stringency_index
-        #population
-        #population_density
-        #median_age
-        #aged_65_older
-        #aged_70_older
-        #gdp_per_capita
-        #extreme_poverty
-        #cvd_death_rate
-        #diabetes_prevalence
-        #female_smokers
-        #male_smokers
-        #handwashing_facilities
-        #hospital_beds_per_100k
+
